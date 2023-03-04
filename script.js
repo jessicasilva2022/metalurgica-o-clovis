e28f36bb144cdb5cc906b5f4d48de78dd5244ccd
Jéssica <102984160+jessicasilva2022@users.noreply.github.com>
2023-03-03 21:05:06 -0300

    O andre mudou a function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function goToWhatsapp()
+function goToWhatsapp(form)
 {
-	location.replace("https://wa.me/5585985243868?text=Oi,%20%0ADiga%20o%20que%20voc%C3%AA%20precisa?!");
+	var input = form.Email.value;
+	// alert(input);
+	location.assign("https://wa.me/5585985243868?text=Oi,%20%0ADiga%20o%20que%20voc%C3%AA%20precisa?!"+input);
 }
