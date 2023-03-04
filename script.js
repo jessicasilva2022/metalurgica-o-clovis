e111b630be0ff5f8fd278db320cbf9cf2af113ba
Jéssica <102984160+jessicasilva2022@users.noreply.github.com>
2023-03-03 21:22:52 -0300

    habilitei o alert

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function goToWhatsapp(form)
 {
 	var input = form.Email.value;
-	// alert(input);
+	alert(input);
 	location.assign("https://wa.me/5585985243868?text=Oi,%20%0ADiga%20o%20que%20voc%C3%AA%20precisa?!"+input);
 }
