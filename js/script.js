0dda01fdc67c5c2b1302264f1913f26e6a223446
Jéssica <102984160+jessicasilva2022@users.noreply.github.com>
2023-03-06 16:09:13 -0300

    section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ function goToWhatsapp(form)
 	alert(input);
 	location.assign("https://wa.me/5585985243868?text=Oi,%20%0ADiga%20o%20que%20voc%C3%AA%20precisa?!"+input);
 }
+
+function name(form)
+{
+	document.getElementById("name");
+
+	if(nome.value !="")
+	{
+		alert('Sr.(a)' + nome.value +'foi enviado para o zap');
+	}
+
+}
